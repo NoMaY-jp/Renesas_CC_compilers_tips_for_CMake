2899f8b435bedf06d7ea256c7796df10e4a67813
NoMaY (a user of Renesas Rulz Japanese Forum) <NoMaY-jp@outlook.com>
2023-06-07 21:30:09 +0900

    Add/merge test batch files for Visual Studio 2022's Microsoft CMake

--- testall/primary_vs2022_ms_cmake.bat
+++ testall/primary_vs2022_ms_cmake.bat
@@ -5,9 +5,9 @@ if "%1" == "" (
     goto :EOF
 )
 
-set CMAKE=C:\Program Files\CMake\bin\cmake.exe
+set CMAKE=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe
 set TESTROOT=%~dpf1%
-set TESTRESULT=%~dpf1%\testall\primary10
+set TESTRESULT=%~dpf1%\testall\primary_vs2022_ms_cmake
 for /f "usebackq" %%t in (`time /t`) do set TESTSTARTTIME=%%t
 
 echo\
@@ -18,6 +18,15 @@ call :build_folder_check tb_rx65n
 call :build_folder_check rl78g23_fpb
 call :build_folder_check RH850F1KM_SampleProject
 
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex1
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex2
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex2xx
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex3
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex4
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex4xx
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex5
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex6
+call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex6xx
 call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex11
 call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex12
 call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex12xx
@@ -28,6 +37,15 @@ call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex15
 call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex16
 call :testall_folder_check tb_rx65n renesas-rx-ccrx-toolchain-ex16xx
 
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex1
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex2
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex2xx
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex3
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex4
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex4xx
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex5
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex6
+call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex6xx
 call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex11
 call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex12
 call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex12xx
@@ -38,6 +56,12 @@ call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex15
 call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex16
 call :testall_folder_check rl78g23_fpb renesas-rl78-ccrl-toolchain-ex16xx
 
+call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex1
+call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex2
+call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex3
+call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex4
+call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex5
+call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex6
 call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex11
 call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex12
 call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex13
@@ -45,6 +69,15 @@ call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-
 call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex15
 call :testall_folder_check RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex16
 
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex1    x mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex2    x mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex2xx  x mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex3    x mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex4    x mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex4xx  elf mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex5    x mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex6    x mot
+call :run tb_rx65n renesas-rx-ccrx-toolchain-ex6xx  elf mot
 call :run tb_rx65n renesas-rx-ccrx-toolchain-ex11   x mot
 call :run tb_rx65n renesas-rx-ccrx-toolchain-ex12   x mot
 call :run tb_rx65n renesas-rx-ccrx-toolchain-ex12xx x mot
@@ -55,6 +88,15 @@ call :run tb_rx65n renesas-rx-ccrx-toolchain-ex15   x mot
 call :run tb_rx65n renesas-rx-ccrx-toolchain-ex16   x mot
 call :run tb_rx65n renesas-rx-ccrx-toolchain-ex16xx elf mot
 
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex1    x mot
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex2    x mot
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex2xx  x mot
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex3    x mot
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex4    x mot
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex4xx  elf mot
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex5    x mot
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex6    x mot "p,s"
+call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex6xx  elf mot
 call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex11   x mot
 call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex12   x mot
 call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex12xx x mot
@@ -65,6 +107,12 @@ call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex15   x mot
 call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex16   x mot "p,s"
 call :run rl78g23_fpb renesas-rl78-ccrl-toolchain-ex16xx elf mot
 
+call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex1  x mot
+call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex2  x mot
+call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex3  x mot
+call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex4  x mot
+call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex5  x mot
+call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex6  x mot "p,s"
 call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex11 x mot
 call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex12 x mot
 call :run RH850F1KM_SampleProject renesas-rh850-ccrh-toolchain-ex13 x mot
@@ -113,7 +161,7 @@ if "%~1" == "rl78g23_fpb" if exist %TESTROOT%\rl78g23_fpb\src\smc_gen\Config_SMS
     echo\
 )
 mkdir %TESTROOT%\%1\build
-set CMDLINE="%CMAKE%" --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G Ninja
+set CMDLINE="%CMAKE%" --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DRENESAS_INTELLISENSE_HELPER_EXTRA_FLAGS:STRING="-include\"%TESTROOT%/%1/.vs/c_cpp_intellisense_helper.h\"" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G Ninja
 echo %CMDLINE%
 %CMDLINE%
 if errorlevel 1 (
@@ -146,7 +194,7 @@ if not exist "%TESTROOT%\%1\build\%1.%4" (
     goto :fail
 )
 if not "%~5" == "p,s" goto Ninja_skip_p_s
-    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=1 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G Ninja
+    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=1 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DRENESAS_INTELLISENSE_HELPER_EXTRA_FLAGS:STRING="-include\"%TESTROOT%/%1/.vs/c_cpp_intellisense_helper.h\"" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G Ninja
     echo %CMDLINE%
     %CMDLINE%
     set CMDLINE="%CMAKE%" --build %TESTROOT%\%1\build --config RelWithDebInfo --target all --
@@ -166,7 +214,7 @@ if not "%~5" == "p,s" goto Ninja_skip_p_s
         goto :fail
     )
 
-    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=2 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G Ninja
+    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=2 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DRENESAS_INTELLISENSE_HELPER_EXTRA_FLAGS:STRING="-include\"%TESTROOT%/%1/.vs/c_cpp_intellisense_helper.h\"" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G Ninja
     echo %CMDLINE%
     %CMDLINE%
     set CMDLINE="%CMAKE%" --build %TESTROOT%\%1\build --config RelWithDebInfo --target all --
@@ -205,7 +253,7 @@ if "%~1" == "rl78g23_fpb" if exist %TESTROOT%\rl78g23_fpb\src\smc_gen\Config_SMS
     echo\
 )
 mkdir %TESTROOT%\%1\build
-set CMDLINE="%CMAKE%" --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G "Unix Makefiles"
+set CMDLINE="%CMAKE%" --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DRENESAS_INTELLISENSE_HELPER_EXTRA_FLAGS:STRING="-include\"%TESTROOT%/%1/.vs/c_cpp_intellisense_helper.h\"" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G "Unix Makefiles"
 echo %CMDLINE%
 %CMDLINE%
 if errorlevel 1 (
@@ -238,7 +286,7 @@ if not exist "%TESTROOT%\%1\build\%1.%4" (
     goto :fail
 )
 if not "%~5" == "p,s" goto UnixMakefile_skip_p_s
-    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=1 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G "Unix Makefiles"
+    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=1 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DRENESAS_INTELLISENSE_HELPER_EXTRA_FLAGS:STRING="-include\"%TESTROOT%/%1/.vs/c_cpp_intellisense_helper.h\"" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G "Unix Makefiles"
     echo %CMDLINE%
     %CMDLINE%
     set CMDLINE="%CMAKE%" --build %TESTROOT%\%1\build --config RelWithDebInfo --target all --
@@ -258,7 +306,7 @@ if not "%~5" == "p,s" goto UnixMakefile_skip_p_s
         goto :fail
     )
 
-    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=2 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G "Unix Makefiles"
+    set CMDLINE="%CMAKE%" --no-warn-unused-cli -DEXAMPLE_ALT_OUTPUT_TYPE=2 -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DRENESAS_INTELLISENSE_HELPER_EXTRA_FLAGS:STRING="-include\"%TESTROOT%/%1/.vs/c_cpp_intellisense_helper.h\"" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_TOOLCHAIN_FILE:FILEPATH=%TESTROOT%\%1\cmake\%2.cmake -S=%TESTROOT%\%1 -B=%TESTROOT%\%1\build -G "Unix Makefiles"
     echo %CMDLINE%
     %CMDLINE%
     set CMDLINE="%CMAKE%" --build %TESTROOT%\%1\build --config RelWithDebInfo --target all --
